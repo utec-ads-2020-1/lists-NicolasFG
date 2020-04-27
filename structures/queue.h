3387998a9231488646affc0509302be46b3a290f
NicolasFG <nicolas.figueroa@utec.edu.pe>
2020-04-26 20:13:39 -0500

    Terminado

--- structures/queue.h
+++ structures/queue.h
@@ -1,28 +1,172 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-using namespace std;
+#include <iostream>
+#include "node.h"
 
-#define MAX 1000 
+#define MAX 1000
+
+
+using namespace std;
 
 // TODO: Implement all methods
 template <typename T>
 class queue {
-	T* data;
-	int top;
-	int capacity;
+
+    Node<T>* header;
+    Node<T>* tailer;
+    int tamanio;
+    int capacity;
 
 public:
-	stack(int size = MAX);
-	~stack();   		
+    queue(int size = MAX);
+    ~queue();
 
-	void push(T);
-	int pop();
-	int front();
+    //Agregar
+    void push(T);
+
+    //Sacar
+    void pop();
+
+    //El proximo que saldra
+    int front();
+
+    //El ultimo que entro
     int back();
 
-	int size();
-	bool empty();
+    //Tamaño de la cola
+    int size();
+
+    //Aumenta el tamaño Capacity
+    void resize();
+
+    //Para saber si esta vacio
+    bool empty();
 };
+
+template<typename T>
+queue<T>::queue(int size) {
+    header = new Node<T>;
+    tailer = new Node<T>;
+
+    header->next = tailer;
+    tailer->prev = header;
+
+    header->prev = nullptr;
+    tailer->next = nullptr;
+
+    capacity = size;
+
+    tamanio = 0;
+
+}
+
+
+template<typename T>
+void queue<T>::push(T elemento) {
+
+    if (tamanio < capacity)
+    {
+        auto *temp = new Node<T>;
+        temp ->data = elemento;
+
+        temp->next = tailer;
+        temp->prev = tailer->prev;
+
+        tailer->prev->next = temp;
+        tailer->prev = temp;
+
+        tamanio++;
+
+    }
+    if (tamanio > capacity){
+        resize();
+
+    }
+
+    else{
+        throw exception();
+    }
+
+}
+
+
+template<typename T>
+void queue<T>::pop() {
+
+    if(!empty()){
+
+        auto *temp = header->next->next;
+
+        delete header->next;
+
+        header->next = temp;
+        temp->prev = header;
+
+        tamanio--;
+
+    }
+    else {
+        throw exception();
+    }
+
+}
+
+template<typename T>
+int queue<T>::front() {
+
+    if (empty())
+    {
+        throw exception();
+    }
+    else {
+        cout << header->next->data;
+        return header->next->data;
+    }
+
+}
+
+template<typename T>
+int queue<T>::back() {
+    if(empty()){
+        throw exception();
+    }
+    else {
+        cout << tailer->prev->data;
+        return tailer-> prev->data;
+    }
+
+}
+
+template<typename T>
+bool queue<T>::empty() {
+    if (tamanio == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+template<typename T>
+queue<T>::~queue() {
+    while(!empty()){
+        pop();
+    }
+    delete header;
+    delete tailer;
+}
+
+template<typename T>
+int queue<T>::size() {
+    return tamanio;
+}
+
+
+template<typename T>
+void queue<T>::resize() {
+    capacity += MAX;
+}
+
 
 #endif
--- structures/stack.h
+++ structures/stack.h
@@ -1,27 +1,119 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <iostream>
+#include "node.h"
+
+#define MAX 1000
+
 using namespace std;
 
-#define MAX 1000 
 
 // TODO: Implement all methods
 template <typename T>
 class stack {
-	T* data;
-	int top;
-	int capacity;
+    T* data;
+    int top;
+    int capacity;
 
 public:
-	stack(int size = MAX);
-	~stack();   		
+    stack(int size = MAX);
+    ~stack();
 
-	void push(T);
-	int pop();
-	int top();
+    void push(T);
+    void pop();
+    int Top();
 
-	int size();
-	bool empty();
+    void resize();
+    int size();
+    bool empty();
+
 };
+
+template<typename T>
+stack<T>::stack(int size) {
+    data = nullptr;
+    capacity = MAX;
+    top = -1;
+}
+
+template<typename T>
+stack<T>::~stack() {
+    delete data;
+}
+
+
+//Listo
+template<typename T>
+void stack<T>::push(T elemento) {
+    data = new T[MAX];
+
+    if (empty()){
+        throw exception();
+    }
+    if (size() > capacity){
+        resize();
+    }
+    else{
+        data[++top] = elemento;
+        cout << "Elemento insertado: " << elemento << endl;
+    }
+}
+
+//Listo
+template<typename T>
+int stack<T>::Top() {
+
+    if (top == -1){
+        throw exception();
+    }
+
+    else {
+        cout << "El indice del ultimo elemento insertado es: " << top;
+        return top;
+    }
+}
+
+
+//Listo
+template<typename T>
+void stack<T>::pop() {
+    if (empty()) {
+
+        throw exception();
+    }
+    else {
+        cout << "Elemento eliminado: " << data[top];
+    }
+}
+
+//Listo
+template<typename T>
+bool stack<T>::empty() {
+
+    if (data == nullptr){
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
+
+//Listo
+template<typename T>
+int stack<T>::size() {
+    int tamanio = top + 1;
+    cout << "El tamanio de la pila es: " << tamanio << " // ";
+    return tamanio;
+
+}
+
+template<typename T>
+void stack<T>::resize() {
+    capacity += MAX;
+}
+
+
 
 #endif
